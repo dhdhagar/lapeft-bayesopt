8e99c0c30e1b09853338d03f26f7689767d4d16a
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-03-28 01:12:38 -0400

    Fix

--- examples/run_fixed_features_20q_research.py
+++ examples/run_fixed_features_20q_research.py
@@ -29,7 +29,7 @@ def main():
         'opt_val': pd_dataset['Similarity'].max()
     }
 
-    results = run_bayesopt(dataset, n_init_data=5, T=26, randseed=9999)
+    results = run_bayesopt(dataset, n_init_data=2, T=26, randseed=9999)
 
     # Plot
     t = np.arange(len(results))
