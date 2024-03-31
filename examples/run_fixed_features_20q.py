0bf19cc1a1fa1d3a4d86428d02caf3fcd90a3ecf
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-03-30 22:00:35 -0400

    Fix

--- examples/run_fixed_features_20q.py
+++ examples/run_fixed_features_20q.py
@@ -340,6 +340,7 @@ if __name__ == '__main__':
     all_results = []
     for i in range(args.n_seeds):
         seed = args.seed + i
+        print(f'\nSeed {seed}:')
         results = run_bayesopt(words=list(pd_dataset['Words']), features=features, targets=targets,
                                test_word=test_word, n_init_data=args.n_init_data, T=args.T, seed=seed,
                                device='cuda' if args.cuda else 'cpu')
@@ -358,12 +359,12 @@ if __name__ == '__main__':
         "results": {
             "trace_y_mean": all_trace_y.mean(axis=0),
             "trace_y_std": all_trace_y.std(axis=0),
-            "n_found": [1 for res in all_results if res["results"]["steps_to_opt"] != -1] / args.n_seeds,
+            "n_found": sum([1 for res in all_results if res["results"]["steps_to_opt"] != -1]) / args.n_seeds,
             "avg_steps_to_opt": np.mean(
                 [res["results"]["steps_to_opt"] for res in all_results if res["results"]["steps_to_opt"] != -1]),
             "trace_y_rand_mean": all_trace_y_rand.mean(axis=0),
             "trace_y_rand_std": all_trace_y_rand.std(axis=0),
-            "n_found_rand": [1 for res in all_results if res["results"]["steps_to_opt_rand"] != -1] / args.n_seeds,
+            "n_found_rand": sum([1 for res in all_results if res["results"]["steps_to_opt_rand"] != -1]) / args.n_seeds,
             "avg_steps_to_opt_rand": np.mean(
                 [res["results"]["steps_to_opt_rand"] for res in all_results if
                  res["results"]["steps_to_opt_rand"] != -1]),
