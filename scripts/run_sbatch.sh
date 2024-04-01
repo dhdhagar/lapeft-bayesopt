9ac343271c6fbc2d2f56c74bfa9c14ee6a708eb2
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-04-01 18:55:20 -0400

    Fix

--- scripts/run_sbatch.sh
+++ scripts/run_sbatch.sh
@@ -2,7 +2,8 @@
 
 echo CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}
 eval "$(conda shell.bash hook)"
-export PYTHONPATH=$(pwd):$PYTHONPATH;
 conda deactivate
 conda activate lapeft
+export PYTHONPATH=$(pwd):$PYTHONPATH;
+export TRANSFORMERS_CACHE=/iesl/canvas/dagarwal/hf_cache
 python "$@"
