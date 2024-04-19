71061e87c6d0eecc84dac021953ff1ad296437f6
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-04-18 21:39:32 -0400

    Fix

--- examples/prompting.py
+++ examples/prompting.py
@@ -7,32 +7,33 @@ class MyPromptBuilder(PromptBuilder):
         self.hint = hint
 
     def get_prompt(self, x: str, obj_str: str, additive: bool = False) -> str:
+        # Sober Look experiments:
         # if self.kind == 'completion':
         #     return f'The estimated {obj_str} of the molecule {x} is: '
         # elif self.kind == 'just-smiles':
         #     return x
+
+        # TwentyQuestions
         instruction = 'The task is to find a hidden test word by guessing new words.'
         next_word = f'The next guess is {x}.'
         hint = self.hint
 
         if self.kind == 'word':
-            # TwentyQuestions
             return [x]
         elif self.kind == 'instruction':
-            # TwentyQuestions
             # return f'The task is to find a hidden test word by guessing new words. What is a word that is similar to {x}?'
             if not additive:
                 return [f'{instruction} {next_word}']
             else:
                 return [instruction, next_word]
         elif self.kind == 'hint':
-            # TwentyQuestions
+            assert self.hint != ""
             if not additive:
                 return [f'{instruction} {hint} {next_word}']
             else:
                 return [instruction, hint, next_word]
         elif self.kind == 'hint-goodness':
-            # TwentyQuestions
+            assert self.hint != ""
             if not additive:
                 return [f'{instruction} {hint} {next_word} Is that a good guess?']
             else:
