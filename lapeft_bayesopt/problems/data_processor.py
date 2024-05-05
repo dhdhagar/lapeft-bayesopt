176cd9a6b2f891719be1031dfaaf30ddc04e729f
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-05-05 15:05:18 -0400

    Fix

--- lapeft_bayesopt/problems/data_processor.py
+++ lapeft_bayesopt/problems/data_processor.py
@@ -36,13 +36,14 @@ class DataProcessor:
         dataset = Dataset.from_pandas(pandas_dataset)
 
         def tokenize(row):
+            out = {}
             prompts = self.prompt_builder.get_prompt(row[self.x_col], self.obj_str, additive=additive)
             out['prompts'] = prompts
             if append_eos:
                 prompts = [prompt + self.tokenizer.eos_token for prompt in prompts]
             if len(prompts) == 1:
                 prompts = prompts[0]
-            out = self.tokenizer(prompts, truncation=True, max_length=max_seq_len, padding=True)
+            out.update(self.tokenizer(prompts, truncation=True, max_length=max_seq_len, padding=True))
             labels = self._get_targets(row)
             if labels is not None:
                 out['labels'] = labels
