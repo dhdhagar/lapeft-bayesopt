c9028bb007d3faa06cb8f6e078e5593e2d9bd183
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2024-05-05 18:07:40 -0400

    Fix

--- examples/run_fixed_features_20q.py
+++ examples/run_fixed_features_20q.py
@@ -220,9 +220,7 @@ def load_features(dataset, test_word, test_idx):
         # Build the textual representation based on the prompt strategy
         prompt_builder = MyPromptBuilder(kind=args.prompt_strategy, hint=args.hint)
         data_processor = TwentyQuestionsDataProcessor(prompt_builder, tokenizer)
-        dataloader = data_processor.get_dataloader(dataset,
-                                                   additive=args.additive_features or is_vtoken_feat_extraction,
-                                                   batch_size=1 if is_vtoken_feat_extraction else 16)
+        dataloader = data_processor.get_dataloader(dataset, additive=args.additive_features or is_vtoken_feat_extraction)
 
         # Get features and target values for each candidate
         features, targets = [], []
